ff5c8a61f67c28247591ba2c0f937d6b2006805f
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-03 19:32:01 +0530

    Update event.js

--- admin/event.js
+++ admin/event.js
@@ -2,6 +2,7 @@ const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
 
 const token = localStorage.getItem("cookie");
 
+const count = document.querySelector(".count");
 const table = document.querySelector(".eventinfo");
 
 
@@ -24,6 +25,30 @@ const admintable = (array) => {
   });
 };
 
+const event = (array) => {
+  if (array.length == 0) return null;
+  var modeMap = {},
+    maxCount = 1,
+    modes = [];
+
+  for (var i = 0; i < array.length; i++) {
+    const {event} = array[i];
+    var el = event;
+
+    if (modeMap[el] == null) modeMap[el] = 1;
+    else modeMap[el]++;
+
+    if (modeMap[el] > maxCount) {
+      modes = [el];
+      maxCount = modeMap[el];
+    } else if (modeMap[el] == maxCount) {
+      modes.push(el);
+      maxCount = modeMap[el];
+    }
+  }
+  return modes;
+}
+
 admintable(info);
 
 if (token) {
@@ -38,7 +63,8 @@ if (token) {
       .then((res) => res.json())
       .then((data) => {
         info = data.data;
-        admintable(data.data);
+       count.innerHTML=`Most popular event is <b>${event(data.data)}</b>`;
+       admintable(data.data);
       })
       .catch((err) => {
         console.log(err);
