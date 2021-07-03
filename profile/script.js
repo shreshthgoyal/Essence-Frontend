5a4183d19275367716f3e2c92cb82b3b64eadd1c
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-03 22:03:36 +0530

    Update script.js

--- profile/script.js
+++ profile/script.js
@@ -1,18 +1,20 @@
 const div = document.querySelector(".div");
 const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
 
+
+const list = document.getElementById("myPopup1")
 const token = localStorage.getItem("cookie");
 
 const dashboard = (array) => {
-    div.innerHTML = "";
-    
-    array.forEach((info) => {
-      const { name, college, contact } = info;
-  
-      const profile = document.createElement("section");
-      profile.classList.add("profile-info");
- 
-      const insideHtml = ` <h1 class="first-name"> ${name} </h1> 
+  div.innerHTML = "";
+
+  array.forEach((info) => {
+    const { name, college, contact } = info;
+
+    const profile = document.createElement("section");
+    profile.classList.add("profile-info");
+
+    const insideHtml = ` <h1 class="first-name"> ${name} </h1> 
       <p>Welcome to Essence, This is your dashboard. Here at Essence our users matters us the most!💜</p>
       <h2>Information</h2>
      <p>
@@ -21,35 +23,129 @@ const dashboard = (array) => {
           Contact : ${contact}
           <br>
       </p>`;
-  
-      profile.innerHTML = insideHtml;
-  
-      div.appendChild(profile);
-    });
-  };
 
-  if(token){
-fetch(`${apiUrl}/user/dashboard`,
+    profile.innerHTML = insideHtml;
+
+    div.appendChild(profile);
+  });
+};
+
+const event = (array) => {
+
+  array.forEach((info) => {
+    const { event } = info;
+
+    const insideHtml = `${event}<br>`;
+
+    list.innerHTML += insideHtml;
+  });
+};
+
+
+const goodies = (array) => {
+  const list = document.getElementById("myPopup2")
+  array.forEach((info) => {
+    const { goodie } = info;
+    const insideHtml = `${goodie}<br>`;
+    list.innerHTML += insideHtml;
+  });
+};
+
+const pronite = (array) => {
+  const list = document.getElementById("myPopup3")
+  array.forEach((info) => {
+    const { pronite } = info;
+    const insideHtml = `${pronite}<br>`;
+    list.innerHTML += insideHtml;
+  });
+};
+
+
+if (token) {
+  fetch(`${apiUrl}/user/dashboard`,
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `${localStorage.getItem("cookie")}`,
+        "Authorization": token,
       },
-      credentials: 'include',
+      credentials: 'same-origin'
     })
-      .then((res) => res.json())
-      .then((data) => {
-        const user = data.data;
-        dashboard(user);
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
+    .then((res) => res.json())
+    .then((data) => {
+      const user = data.data;
+      dashboard(user);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 
-    else
+  fetch(`${apiUrl}/user/event`,
     {
-      location.href = "https://essencefest.netlify.app/";
-      alert("Please sign in first");
-    }
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token,
+      },
+      credentials: 'same-origin'
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      event(data.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  fetch(`${apiUrl}/user/goodies`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token,
+      },
+      credentials: 'same-origin'
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      goodies(data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  fetch(`${apiUrl}/user/pronite`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token,
+      },
+      credentials: 'same-origin'
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      pronite(data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+
+}
+
+if (!token) {
+  location.href = "https://essencefest.netlify.app/";
+  alert("Please sign in first");
+}
+
+function myFunction1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
