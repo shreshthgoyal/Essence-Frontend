d9cee82b4a6dfae1b92196f94090ecc8d37d9f69
kshitij-404 <76985867+kshitij-404@users.noreply.github.com>
2021-07-06 00:53:41 +0530

    Update index.js

--- goodies/index.js
+++ goodies/index.js
@@ -34,3 +34,69 @@ function buyAnimate(i) {
 for (i = 1; i <= 6; i++) {
   buyAnimate(i);
 }
+
+const buyButton = [];
+const goodieid = [];
+
+var j;
+
+for (j = 1; j <= 6; j++) {
+  buyButton[j] = document.querySelector(".buy" + j);
+}
+
+const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
+
+const unique =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN5bGF0cG1jOHhAYnV5LWJsb2cuY29tIiwiaWF0IjoxNjI1Mzg2MTI2fQ.1e8AujLUg4-IrliEq7Jr1Z88bioaYVVfQ8y95fkzIb8";
+
+localStorage.setItem("cookie", unique);
+
+const token = localStorage.getItem("cookie");
+
+const url = window.location.href;
+
+var k;
+
+function goodie_reg(k) {
+  const purchaseGoodies = (route, fetch_method) => {
+    console.log(k);
+    fetch(`${apiUrl}/goodies/${route}/${k}`, {
+      method: `${fetch_method}`,
+      crossDomain: true,
+      xhrFields: {
+        withCredentials: true,
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    }).then((res) => {
+      const status = res.status;
+      res.json().then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert(data.message);
+        }
+
+        if (status === 401) {
+          location.href = "../RegisterLogin/";
+        } else {
+          location.href = url;
+        }
+      });
+    });
+  };
+
+  buyButton[k].addEventListener("click", (event) => {
+    event.preventDefault();
+    const route = "goodiepurchase";
+    const fetch_method = "POST";
+
+    purchaseGoodies(route, fetch_method);
+  });
+}
+
+for (k = 1; k <= 6; k++) {
+  goodie_reg(k);
+}
