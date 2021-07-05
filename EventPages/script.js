619df7008b2c09f55a14c6bba2317ab445e4b0bb
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-06 01:27:10 +0530

    Add files via upload

---
+++ EventPages/script.js
@@ -0,0 +1,70 @@
+const registerButton = document.querySelector(".register-button");
+const unregisterButton = document.querySelector(".unregister-button");
+const body = document.querySelector("body");
+
+const eventid = body.id;
+
+const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
+
+
+
+const token = localStorage.getItem("cookie");
+
+const url = window.location.href;
+
+
+const register_unregister = (route, fetch_method) => {
+    fetch(`${apiUrl}/events/${route}/${eventid}`, {
+        method: `${fetch_method}`,
+        crossDomain: true,
+        xhrFields: {
+            withCredentials: true
+               },
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization" : token
+        },
+    })
+    .then((res) => {
+        const status = res.status;
+        res.json()
+        .then((data) => {
+        
+        
+        if (data.error){
+            alert(data.error);
+        }
+        else{
+            alert(data.message);    
+        }
+        
+        if(status === 401){
+            location.href = "../RegisterLogin/";
+        }
+        else{
+            location.href = url;
+        }
+        })
+    });
+};
+
+registerButton.addEventListener("click",(event) => {
+    event.preventDefault();
+    const route = "eventregn";
+    const fetch_method = "POST";
+
+    register_unregister(route, fetch_method);
+    
+
+    
+});
+
+unregisterButton.addEventListener("click",(event) => {
+    event.preventDefault();
+
+    const route = "eventunregister";
+    const fetch_method = "DELETE";
+
+    register_unregister(route, fetch_method);
+
+});
