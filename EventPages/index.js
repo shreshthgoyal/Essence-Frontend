1a6ec58ae55f683a1ab5fc1fc7db37d1cc1a9c53
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-08 04:02:34 +0530

    Update index.js

--- EventPages/index.js
+++ EventPages/index.js
@@ -10,7 +10,7 @@ window.onscroll = function () {
   }
 };
 
-back.addEventListener("click", ()=>{
+back.addEventListener("click", ()=>{                                                        //takes the user to the previous location
   window.history.back();
 })
 
@@ -26,20 +26,20 @@ function Envelope() {
   this.messageQuote = document.querySelector(".quote");
   this.messageAuthor = document.querySelector(".author");
 
-  this.openEnvelope = function () {
+  this.openEnvelope = function () {                                                                           //function to trigger the envelope opening animation
     this.envelopeOpening.classList.toggle("open-envelope");
     this.envelopeMessage.classList.toggle("push-message");
     this.envelopeGlow.classList.toggle("envelope-glow");
     this.envelopeMessageArea.classList.toggle("envelope-msg-area-show");
   };
 
-  this.showMessage = function () {
+  this.showMessage = function () {                                                                            //function to trigger the animation when user wants to read the message
     this.displayMessage.classList.add("view-message");
     this.messageQuote.style.fontSize = "20px";
     this.messageAuthor.style.fontSize = "16px";
   };
 
-  this.closeMessage = function () {
+  this.closeMessage = function () {                                                                           //function to close the message
     this.displayMessage.classList.remove("view-message");
     this.messageQuote.style.fontSize = "0";
     this.messageAuthor.style.fontSize = "0";
