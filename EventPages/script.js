8e71844a558471e530f4fa677e6c042b2ea7b06c
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-07 18:41:25 +0530

    Update script.js

--- EventPages/script.js
+++ EventPages/script.js
@@ -1,17 +1,40 @@
-const registerButton = document.querySelector(".register-button");
-const unregisterButton = document.querySelector(".unregister-button");
+const regnButton = document.querySelector(".eventregn");
+
 const body = document.querySelector("body");
 
 const eventid = body.id;
 
 const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
 
-
+const eventName = document.querySelector(".event_name").innerHTML;
 
 const token = localStorage.getItem("cookie");
 
 const url = window.location.href;
 
+if (token) {
+    fetch(`${apiUrl}/user/event`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": token,
+        },
+        credentials: 'same-origin'
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        
+        (data.data).forEach(element => {
+            if(element.event === eventName){
+                regnButton.innerHTML = "Unregister";
+            }
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
 const register_unregister = (route, fetch_method) => {
     fetch(`${apiUrl}/events/${route}/${eventid}`, {
@@ -52,23 +75,25 @@ const register_unregister = (route, fetch_method) => {
     });
 };
 
-registerButton.addEventListener("click",(event) => {
+regnButton.addEventListener("click",(event) => {
     event.preventDefault();
-    const route = "eventregn";
-    const fetch_method = "POST";
 
-    register_unregister(route, fetch_method);
-    
+    if(regnButton.innerHTML === "Register"){
+        console.log("Registering");
+        const route = "eventregn";
+        const fetch_method = "POST";
+        regnButton.innerHTML = "Unregister";
+        register_unregister(route, fetch_method);
+    }
 
+    else if (regnButton.innerHTML === "Unregister"){
+        console.log("Unregistering");
+        const route = "eventunregister";
+        const fetch_method = "DELETE";
+        regnButton.innerHTML = "Register";
+        register_unregister(route, fetch_method);
+    }
     
 });
 
-unregisterButton.addEventListener("click",(event) => {
-    event.preventDefault();
 
-    const route = "eventunregister";
-    const fetch_method = "DELETE";
-
-    register_unregister(route, fetch_method);
-
-});
