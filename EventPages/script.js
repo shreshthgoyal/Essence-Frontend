13509bf6fe63e7c2a3944846d80d951f6331c0bf
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-07 19:18:20 +0530

    Update script.js

--- EventPages/script.js
+++ EventPages/script.js
@@ -24,7 +24,7 @@ const url = window.location.href;
       })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.data);
+        console.log(data.data[4].event);
         for(var i = 0; i < data.data.length; i++) {
             if(data.data[i].event === eventName){
                 regnButton.innerHTML = "Unregister";
