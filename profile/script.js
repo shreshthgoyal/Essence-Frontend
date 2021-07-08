5b679a0774aa9383ac45f9b3a42e50e66ca60710
kshitij-404 <76985867+kshitij-404@users.noreply.github.com>
2021-07-09 02:35:22 +0530

    Update script.js

--- profile/script.js
+++ profile/script.js
@@ -1,6 +1,6 @@
 const div = document.querySelector(".div");
 const apiUrl = "https://gentle-thicket-19334.herokuapp.com";
-const list = document.getElementById("myPopup1")
+const list = document.getElementById("myPopup1");
 const token = localStorage.getItem("cookie");
 const e = document.querySelector(".e");
 const p = document.querySelector(".p");
@@ -8,11 +8,10 @@ const g = document.querySelector(".g");
 const logoutButton = document.querySelector(".logout-button");
 let back = document.querySelector(".back");
 
-back.addEventListener("click", () => {                                                                          //takes the user to previous location
+back.addEventListener("click", () => {
 
   location.href = "../";
-})
-
+});
 
 const dashboard = (array) => {
   div.innerHTML = "";
@@ -22,8 +21,8 @@ const dashboard = (array) => {
 
     const profile = document.createElement("section");
     profile.classList.add("profile-info");
-                                                                                                                 //display all the user details fetched from backend
-    const insideHtml = ` <h1 class="first-name"> ${name} </h1>                                                        
+
+    const insideHtml = ` <h1 class="first-name"> ${name} </h1> 
       <p>Welcome to Essence, This is your dashboard. Here at Essence our users matters us the most!💙</p>
       <h2>Information</h2>
      <p>
@@ -40,121 +39,118 @@ const dashboard = (array) => {
 };
 
 const event = (array) => {
-
   if (array.length === 0)
-      e.innerHTML = `<a href="https://essencefest.netlify.app/#timeline_anchor" class="btn">Register to Events</a>`
+    e.innerHTML = `<a href="https://essencefest.netlify.app/#timeline_anchor" class="btn">Register to Events</a>`;
 
   array.forEach((info) => {
-    const { event } = info;                                                                                       //adding all the events the user has registered in to the list
+    const { event } = info; //adding all the events the user has registered in to the list
 
     const insideHtml = `${event}<br>`;
     list.innerHTML += insideHtml;
-    
   });
 };
 
-
-
-
 const goodies = (array) => {
   if (array.length === 0)
-  g.innerHTML = `<a href="../goodies" class="btn">Buy some Goodies</a>`
-  const list = document.getElementById("myPopup2")
+    g.innerHTML = `<a href="../goodies" class="btn">Buy some Goodies</a>`;
+  const list = document.getElementById("myPopup2");
   array.forEach((info) => {
     const { goodie } = info;
-    const insideHtml = `${goodie}<br>`;                                                                         //adding all the goodies the user has bought to the list
+    const insideHtml = `${goodie}<br>`; //adding all the goodies the user has bought to the list
     list.innerHTML += insideHtml;
-   
-
   });
 };
 
 const pronite = (array) => {
   if (array.length === 0)
-  p.innerHTML = `<a href="https://essencefest.netlify.app/#pronites_anchor" class="btn">Register to Pronites</a>`
-  const list = document.getElementById("myPopup3")
+    p.innerHTML = `<a href="https://essencefest.netlify.app/#pronites_anchor" class="btn">Register to Pronites</a>`;
+  const list = document.getElementById("myPopup3");
   array.forEach((info) => {
     const { pronite } = info;
-    const insideHtml = `${pronite}<br>`;                                                                        //adding all the pronites the user has registered in to the list
+    const insideHtml = `${pronite}<br>`; //adding all the pronites the user has registered in to the list
     list.innerHTML += insideHtml;
-
   });
 };
 
-
 if (token) {
-  fetch(`${apiUrl}/user/dashboard`,                                   //fetching all the data about the user by making a GET request
+  fetch(
+    `${apiUrl}/user/dashboard`, //fetching all the data about the user by making a GET request
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": token,
+        Authorization: token,
       },
-      credentials: 'same-origin'
-    })
+      credentials: "same-origin",
+    }
+  )
     .then((res) => res.json())
     .then((data) => {
       const user = data.data;
       dashboard(user);
     })
     .catch((err) => {
-      alert("Error Occured")
-    })
+      alert("Error Occured");
+    });
 
-  fetch(`${apiUrl}/user/event`,                                             //fetching the list of events the user is registered in by making a GET request
+  fetch(
+    `${apiUrl}/user/event`, //fetching the list of events the user is registered in by making a GET request
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": token,
+        Authorization: token,
       },
-      credentials: 'same-origin'
-    })
+      credentials: "same-origin",
+    }
+  )
     .then((res) => res.json())
     .then((data) => {
       event(data.data);
     })
     .catch((err) => {
-      alert("Error Occured")
-    })
-  fetch(`${apiUrl}/user/goodies`,                                                 //fetching the list of goodies the user has bought by making a GET request
+      alert("Error Occured");
+    });
+  fetch(
+    `${apiUrl}/user/goodies`, //fetching the list of goodies the user has bought by making a GET request
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": token,
+        Authorization: token,
       },
-      credentials: 'same-origin'
-    })
+      credentials: "same-origin",
+    }
+  )
     .then((res) => res.json())
     .then((data) => {
-      goodies(data.data)
+      goodies(data.data);
     })
     .catch((err) => {
-      alert("Error Occured")
-    })
-  fetch(`${apiUrl}/user/pronite`,                                                     //fetching the list of pronite shows the user is registered in by making a GET request
+      alert("Error Occured");
+    });
+  fetch(
+    `${apiUrl}/user/pronite`, //fetching the list of pronite shows the user is registered in by making a GET request
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": token,
+        Authorization: token,
       },
-      credentials: 'same-origin'
-    })
+      credentials: "same-origin",
+    }
+  )
     .then((res) => res.json())
     .then((data) => {
-      pronite(data.data)
+      pronite(data.data);
     })
     .catch((err) => {
-      alert("Error Occured")
-    })
-
-
+      alert("Error Occured");
+    });
 }
 
 if (!token) {
-  location.href = "/RegisterLogin";                                               //redirect to login page if there is no token, that is if user is not logged in
+  location.href = "/RegisterLogin"; //redirect to login page if there is no token, that is if user is not logged in
 }
 
 //functions to display the events, goodies and pronites list when clicked
@@ -174,6 +170,6 @@ function myFunction3() {
 
 logoutButton.addEventListener("click", (e) => {
   e.preventDefault();
-  localStorage.removeItem("cookie");                                                //log out by removing cookie from local storage
+  localStorage.removeItem("cookie"); //log out by removing cookie from local storage
   location.href = "../";
-})
+});
