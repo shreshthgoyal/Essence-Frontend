e10134d97cb9558c9cca3f4febcf7f69ac0aeec4
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-09 20:44:48 +0530

    Update script.js

--- faqpage/script.js
+++ faqpage/script.js
@@ -18,3 +18,17 @@ details.forEach((targetDetail) => {
     });
   });
 });
+
+//User panel
+
+const navin = document.querySelector(".in");
+
+if (token) {
+  navin.innerHTML = `<i class="fas fa-user"></i>
+  <span>Profile</span>`;
+}
+
+if (!token) {
+  navin.innerHTML = `<i class="fas fa-user"></i>
+  <span>Sign-in</span>`;
+}
